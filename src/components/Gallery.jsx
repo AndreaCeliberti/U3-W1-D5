fb338b8809e081da4a388dfd202a154ab8986e67
AndreaCeliberti <andrea.celiberti2@gmail.com>
2025-10-03 16:09:28 +0200

    gallery complete

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,49 +3,53 @@ import { Col, Container, Row } from "react-bootstrap";
 
 class Gallery extends Component {
   state = {
-    moviesList: [],
+    moviesList: {}, // oggetto con i film per ogni saga
   };
 
   fetchMovies = () => {
-    fetch(`http://www.omdbapi.com/?s=star-wars&apikey=1bacd3b7`)
-      .then((resp) => {
-        if (resp.ok) {
-          return resp.json();
-        } else {
-          throw new Error("Dati non recuperati");
-        }
+    const movieName = {
+      "Lord of the Rings": "lord-of-the-rings",
+      "Star Wars": "star-wars",
+      "Harry Potter": "harry-potter",
+    };
+
+    Promise.all(
+      Object.entries(movieName).map(([name, query]) =>
+        fetch(`http://www.omdbapi.com/?s=${query}&apikey=1bacd3b7`)
+          .then((resp) => {
+            if (resp.ok) return resp.json();
+            throw new Error("Dati non recuperati");
+          })
+          .then((data) => [name, data.Search])
+      )
+    )
+      .then((results) => {
+        const moviesList = Object.fromEntries(results);
+        this.setState({ moviesList });
       })
-      .then((movies) => {
-        console.log("risposta api", movies);
-        this.setState({
-          moviesList: movies.Search,
-        });
-      })
-      .catch((e) => {
-        console.log("errore", e);
-      });
+      .catch((e) => console.log("errore", e));
   };
 
   componentDidMount() {
-    console.log("questo è il didmount");
     this.fetchMovies();
   }
 
   render() {
     console.log("questo è render");
     return (
-      <Container style={{ color: "white" }} className="bg-black d-flex mw-75 m-auto">
-        {this.state.moviesList.map((film) => {
-          return (
-            <>
-              <Row className="justify-content-center">
-                <Col xs={12} sm={6} md={4} xl={2} className=" p-0">
-                  <img src={film.Poster} alt="" />
+      <Container style={{ color: "white" }} className="bg-black d-flex flex-column overflow-x-hidden mw-75 m-auto">
+        {Object.entries(this.state.moviesList).map(([category, films]) => (
+          <div key={category} className="mb-5">
+            <h3 className="">{category}</h3>
+            <Row className="justify-content-center">
+              {films.map((film) => (
+                <Col key={film.imdbID} xs={12} sm={6} md={4} xl={2} className="p-0">
+                  <img src={film.Poster} alt={film.Title} className="img-fluid" />
                 </Col>
-              </Row>
-            </>
-          );
-        })}
+              ))}
+            </Row>
+          </div>
+        ))}
       </Container>
     );
   }
